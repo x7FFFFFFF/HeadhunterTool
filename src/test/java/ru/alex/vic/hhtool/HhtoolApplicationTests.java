13ed287e26617b6cfc136dd1d61d05b474d9d279
Alex <paramonovalexv@gmail.com>
2019-01-26 10:55:21 +0300

    init

---
+++ src/test/java/ru/alex/vic/hhtool/HhtoolApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.alex.vic.hhtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HhtoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
